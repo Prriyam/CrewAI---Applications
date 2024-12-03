569a52d9e7d278054a4ab522b3f6d3102fd68a17
Prriyamvradha Parthasarathi <136329666+Prriyam@users.noreply.github.com>
2024-12-03 11:19:36 -0600

    Update travel_recommendation.py

--- travel_recommendation.py
+++ travel_recommendation.py
@@ -1,3 +1,6 @@
+import sys
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from typing import Dict, List
 from crewai import Agent, Task, Crew, Process
 from crewai.tools import BaseTool
